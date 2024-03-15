d883749c6a1b73ff377aaced45dcdf75970c0d89
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-03-15 13:53:32 +0100

    Create a database model

---
+++ FinalProject-Bollean/Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinalProject_Bollean.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("tag")]
+        public string? Tag { get; set; }
+
+        [Required]
+        [Column("content")]
+        public string? Content { get; set; }
+
+        [Required]
+        [Column("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [Column("userId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
+    }
+}
