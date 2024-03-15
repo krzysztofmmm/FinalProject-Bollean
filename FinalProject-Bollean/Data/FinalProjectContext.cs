ec5bcfed27debab767a37882d7fd1b1133649967
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-03-15 13:54:02 +0100

    Create DbContext file

---
+++ FinalProject-Bollean/Data/FinalProjectContext.cs
@@ -0,0 +1,26 @@
+﻿using FinalProject_Bollean.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject_Bollean.Data
+{
+    public class FinalProjectContext : DbContext
+    {
+        public FinalProjectContext(DbContextOptions<FinalProjectContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Minimal configuration for the User-Post relationship
+            // May need update if needed
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId);
+        }
+    }
+}
