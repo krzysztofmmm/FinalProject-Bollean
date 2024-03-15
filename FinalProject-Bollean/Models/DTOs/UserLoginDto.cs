81bbaecf930ada83808861c091c5de1b1152cc8e
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-03-15 13:54:11 +0100

    Create DTOs

---
+++ FinalProject-Bollean/Models/DTOs/UserLoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject_Bollean.Models.DTOs
+{
+    public class UserLoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
---
+++ FinalProject-Bollean/Models/DTOs/UserRegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject_Bollean.Models.DTOs
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100 , MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
