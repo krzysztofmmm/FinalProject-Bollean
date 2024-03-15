0abe0df1be1147cead647c96bdfc7cf6ee6f96dc
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-03-15 13:53:09 +0100

    Install necessary packeges

--- FinalProject-Bollean/Program.cs
+++ FinalProject-Bollean/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FinalProjectContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
