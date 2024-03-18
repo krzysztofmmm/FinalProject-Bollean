04a0acbd558b5f84095a65d9914c5bbc9045f130
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-03-18 12:35:22 +0100

    Create Dtos for posts

---
+++ FinalProject-Bollean/Models/DTOs/PostCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject_Bollean.Models.DTOs
+{
+    public class PostCreateDto
+    {
+        public int UserId { get; set; }
+        public string? Title { get; set; } // Optional
+        public string? Tag { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+
+}
---
+++ FinalProject-Bollean/Models/DTOs/PostUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject_Bollean.Models.DTOs
+{
+    public class PostUpdateDto
+    {
+        public string? Title { get; set; } // Optional
+        public string? Tag { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
